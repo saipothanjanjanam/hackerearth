7afda906dd7b674d1803ad780c8f2df86d4792e5
Sai Pothan Janjanam <saipothan.janjanam@gmail.com>
2019-01-22 21:42:26 +0530

    Adding datastructure codes

---
+++ data-structures/arrays/1-d/microAndArrayUpdate.cpp
@@ -0,0 +1,36 @@
+/*  
+	Author: Sai Pothan Janjanam
+    Github: github.com/saipothanjanjanam	
+	HackerRank: hackerearth.com/saipothanjanjanam
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int T, N, k, count=0;
+	int A[100000] = {0};
+	cin >> T;
+
+	for (int i = 0; i < T; i++){
+		cin >> N >> K;
+		for (int j = 0; j < N; j++){
+			cin >> A[j];
+		}
+		while(1){
+			for (int j = 0; j < N; j++){
+				cin >> A[j];
+			if (A[j] >= K)
+			{
+				/* code */
+			}
+			}
+
+			count++;
+		}
+		A[100000] = {0};
+	}
+	return 0;
+}
